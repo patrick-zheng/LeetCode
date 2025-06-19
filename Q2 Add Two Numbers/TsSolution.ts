addd6784e05bb71689d43e53f946ccb4e406b550
patrick-zheng <patrickzheng1232@gmail.com>
2025-06-19 09:16:58 -0400

    typos

--- Q2 Add Two Numbers/TsSolution.ts
+++ Q2 Add Two Numbers/TsSolution.ts
@@ -1,11 +1,3 @@
-/*
- * Problem: https://leetcode.com/problems/add-two-numbers/
- * Solution: https://leetcode.com/problems/add-two-numbers/solutions/
- * Time Complexity: O(max(n, m))
- * Space Complexity: O(max(n, m))
- */
-
-
 // Definition for singly-linked list.
 class ListNode {
     val: number
@@ -16,26 +8,23 @@ class ListNode {
     }
 }
 
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const answer: ListNode = new ListNode(0);
+    let current = answer;
+    let carry = 0;
 
-export class Solution {
-    addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-        const answer: ListNode = new ListNode(0);
-        let current = answer;
-        let carry = 0;
+    while (l1 || l2 || carry) {
+        const l1Value = l1 ? l1.val : 0;
+        const l2Value = l2 ? l2.val : 0;
 
-        while (l1 || l2 || carry) {
-            const l1Value = l1 ? l1.val : 0;
-            const l2Value = l2 ? l2.val : 0;
+        const total = l1Value + l2Value + carry;
+        carry = Math.floor(total / 10);
+        current.next = new ListNode(total % 10);
+        current = current.next;
 
-            const total = l1Value + l2Value + carry;
-            carry = Math.floor(total / 10);
-            current.next = new ListNode(total % 10);
-            current = current.next;
-
-            l1 = l1 ? l1.next : null;
-            l2 = l2 ? l2.next : null;
-        }
-
-        return answer.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
     }
+
+    return answer.next;
 }
